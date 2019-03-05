8c958e496d74b420bfdbb630e25f1d30f57f93c2
github username <github email address>
2019-03-05 15:35:52 -0500

    auth

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   resources :collaborations
   resources :administrations
   resources :projects
-  resources :users
-  # get '/', to: 'application#home'
+  resources :users, only: [:show, :new, :create]
+  get '/login', to: 'sessions#new', as: 'login'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy', as: 'logout'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
