4b08a79b4a769469facf9d17433a37830e02d7d2
github username <github email address>
2019-03-08 00:18:09 -0500

    styling besides home

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   has_secure_password
   validates :username, presence: true
   validates :username, uniqueness: true
-
-
+  validates :bio, length: {maximum: 300}
+  # validates :bio, presence: true
 
 end
