d2de6dec5000ed629cfe83403e809f713c531312
bdix1111 <brianna_dixon@rocketmail.com>
2019-03-08 10:29:43 -0500

    more styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,3 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 # Project.destroy_all
-
-Project.create(title: "Project 1", description: "blah blah one blah", completed: true, category: 'potato', tech_stack: 'this will be an array')
-Project.create(title: "Project 2", description: "blah blah two blah", completed: false, category: 'potato', tech_stack: 'this will be an array')
-Project.create(title: "Project 3", description: "blah blah three blah", completed: true, category: 'potato', tech_stack: 'this will be an array')
-Project.create(title: "Project 4", description: "blah blah four blah", completed: true, category: 'potato', tech_stack: 'this will be an array')
-Project.create(title: "Project 5", description: "blah blah five blah", completed: false, category: 'potato', tech_stack: 'this will be an array')
